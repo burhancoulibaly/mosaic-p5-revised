d5e1237373572d4e9e2a095f111bc6d3fc5a053c
burhancoulibaly <burhancoulibaly@gmail.com>
2019-06-30 10:11:32 -0400

    config environment setup complete

--- backend/gcsupload.js
+++ backend/gcsupload.js
@@ -2,7 +2,7 @@ const {Storage} = require('@google-cloud/storage'),
       path = require('path'),
       gcsSharp = require('multer-sharp'),
       gConfig = require("../config/config"),
-      firebaseConf = global.gConfig.firebaseConfig,
+      firebaseConf = global.gConfig.development.firebaseConfig,
       CLOUD_BUCKET = firebaseConf.storageBucket,
       multer = require('multer');
 
