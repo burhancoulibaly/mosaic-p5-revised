47dcffe78cdebb8b613ba2264579e6b297a922fc
burhancoulibaly <burhancoulibaly@gmail.com>
2019-07-31 00:17:36 -0400

    setting up stuff for hosting

--- backend/gcsupload.js
+++ backend/gcsupload.js
@@ -9,7 +9,7 @@ const {Storage} = require('@google-cloud/storage'),
 const storage = new Storage({
   projectId:firebaseConf.projectId,
   credentials:{
-    private_key:Base64.decode(process.env.private_key_base64),
+    private_key:new Buffer(process.env.private_key_base64, 'base64').toString("ascii"),
     client_email:process.env.client_email
   }
 });
@@ -55,7 +55,7 @@ function uploadToGCSMain(req,res,next){
     bucket:CLOUD_BUCKET,
     projectId:storage.projectId,
     credentials:{
-      private_key:Base64.decode(process.env.private_key_base64),
+      private_key:new Buffer(process.env.private_key_base64, 'base64').toString("ascii"),
       client_email:process.env.client_email
     },
     acl: 'publicRead',
@@ -74,7 +74,7 @@ function uploadToGCSMain(req,res,next){
     bucket:CLOUD_BUCKET,
     projectId:storage.projectId,
     credentials:{
-      private_key:Base64.decode(process.env.private_key_base64),
+      private_key:new Buffer(process.env.private_key_base64, 'base64').toString("ascii"),
       client_email:process.env.client_email
     },
     acl: 'publicRead',
